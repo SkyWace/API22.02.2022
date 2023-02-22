110564f18ad86629dc2fc52f47921ef97bd5a6e1
SkyWace <simon.delcloque@hotmail.fr>
2023-02-22 16:07:15 +0100

    UPdate Piece

--- controller/posts.controller.js
+++ controller/posts.controller.js
@@ -3,7 +3,8 @@ const postsController = {
 
     getAll: async (req, res) => {
         try{
-            const [rows, fields] = await pool.query("SELECT date_information, information, type,informations_capteurs.id_capteur, capteurs.nom as nom_capteurs,capteurs.id_maison, maison.nom as nom_maison, superficie, type_capteurs.nom as type_capteurs, unité FROM informations_capteurs INNER JOIN capteurs ON informations_capteurs.id_capteur = capteurs.id INNER JOIN maison ON maison.id = capteurs.id_maison INNER JOIN type_capteurs ON capteurs.type = type_capteurs.id;")
+            const {id_piece} = req.params
+            const [rows, fields] = await pool.query("SELECT date_information, valeur, type, informations_capteurs.id_capteur, capteurs.nom as nom_capteurs,capteurs.id_piece, piece.nom as nom_piece, superficie, type_capteurs.nom as type_capteurs, unité FROM informations_capteurs INNER JOIN capteurs ON informations_capteurs.id_capteur = capteurs.id INNER JOIN piece ON piece.id = capteurs.id_piece INNER JOIN type_capteurs ON capteurs.type = type_capteurs.id WHERE capteur.id_piece = ?" [id_piece])
             res.json({
                 data: rows
         })
@@ -17,7 +18,7 @@ const postsController = {
 
     get_users: async (req, res) => {
         try{
-            const [rows, fields] = await pool.query("SELECT id, utilisateur, nom, prenom, mail, date_creation, id_users, id_maison from users INNER JOIN users_maison ON users.id = users_maison.id_users")
+            const [rows, fields] = await pool.query("SELECT id, utilisateur, nom, prenom, mail, date_creation, id_users, id_piece from users INNER JOIN users_piece ON users.id = users_piece.id_users")
             res.json({
                 data: rows
         })
@@ -46,7 +47,8 @@ const postsController = {
 
     get_information: async (req, res) => {
         try{
-            const [rows, fields] = await pool.query("SELECT ic.date_information, GROUP_CONCAT(ic1.information ORDER BY ic1.id_capteur SEPARATOR ', ') AS informations_type_humidité, GROUP_CONCAT(ic2.information ORDER BY ic2.id_capteur SEPARATOR ', ') AS informations_type_température FROM informations_capteurs ic LEFT JOIN capteurs c1 ON ic.id_capteur = c1.id AND c1.type = 1 LEFT JOIN informations_capteurs ic1 ON ic1.id_capteur = c1.id AND ic.date_information = ic1.date_information LEFT JOIN capteurs c2 ON ic.id_capteur = c2.id AND c2.type = 2 LEFT JOIN informations_capteurs ic2 ON ic2.id_capteur = c2.id AND ic.date_information = ic2.date_information GROUP BY ic.date_information ORDER BY ic.date_information DESC LIMIT 1;")
+            const {id_piece} = req.params
+            const [rows, fields] = await pool.query("SELECT ic.date_information, GROUP_CONCAT(ic1.valeur ORDER BY ic1.id_capteur SEPARATOR ', ') AS valeur_type_humidité, GROUP_CONCAT(ic2.valeur ORDER BY ic2.id_capteur SEPARATOR ', ') AS informations_type_température FROM informations_capteurs ic LEFT JOIN capteurs c1 ON ic.id_capteur = c1.id AND c1.type = 1 LEFT JOIN informations_capteurs ic1 ON ic1.id_capteur = c1.id AND ic.date_information = ic1.date_information LEFT JOIN capteurs c2 ON ic.id_capteur = c2.id AND c2.type = 2 LEFT JOIN informations_capteurs ic2 ON ic2.id_capteur = c2.id AND ic.date_information = ic2.date_information GROUP BY ic.date_information ORDER BY ic.date_information DESC LIMIT 1 WHERE capteur.id_piece = ?;" [id_piece])
             res.json({
                 data: rows
         })
@@ -60,7 +62,8 @@ const postsController = {
 
     get_informations: async (req, res) => {
         try{
-            const [rows, fields] = await pool.query("SELECT ic.date_information, GROUP_CONCAT(ic1.information ORDER BY ic1.id_capteur SEPARATOR ', ') AS informations_type_humidité, GROUP_CONCAT(ic2.information ORDER BY ic2.id_capteur SEPARATOR ', ') AS informations_type_température FROM informations_capteurs ic LEFT JOIN capteurs c1 ON ic.id_capteur = c1.id AND c1.type = 1 LEFT JOIN informations_capteurs ic1 ON ic1.id_capteur = c1.id AND ic.date_information = ic1.date_information LEFT JOIN capteurs c2 ON ic.id_capteur = c2.id AND c2.type = 2 LEFT JOIN informations_capteurs ic2 ON ic2.id_capteur = c2.id AND ic.date_information = ic2.date_information GROUP BY ic.date_information;")
+            const{id_piece} = req.params
+            const [rows, fields] = await pool.query("SELECT ic.date_information, GROUP_CONCAT(ic1.valeur ORDER BY ic1.id_capteur SEPARATOR ', ') AS informations_type_humidité, GROUP_CONCAT(ic2.valeur ORDER BY ic2.id_capteur SEPARATOR ', ') AS informations_type_température FROM informations_capteurs ic LEFT JOIN capteurs c1 ON ic.id_capteur = c1.id AND c1.type = 1 LEFT JOIN informations_capteurs ic1 ON ic1.id_capteur = c1.id AND ic.date_information = ic1.date_information LEFT JOIN capteurs c2 ON ic.id_capteur = c2.id AND c2.type = 2 LEFT JOIN informations_capteurs ic2 ON ic2.id_capteur = c2.id AND ic.date_information = ic2.date_information GROUP BY ic.date_information WHERE capteur.id_piece = ?" [id_piece])
             res.json({
                 data: rows
         })
@@ -75,7 +78,7 @@ const postsController = {
     getById: async (req, res) => {
         try{
             const { id } = req.params
-            const [rows, fields] = await pool.query("SELECT date_information, information, type,informations_capteurs.id_capteur, capteurs.nom as nom_capteurs,capteurs.id_maison, maison.nom as nom_maison, superficie, type_capteurs.nom as type_capteurs, unité FROM informations_capteurs INNER JOIN capteurs ON informations_capteurs.id_capteur = capteurs.id INNER JOIN maison ON maison.id = capteurs.id_maison INNER JOIN type_capteurs ON capteurs.type = type_capteurs.id WHERE capteurs.id= ?", [id])
+            const [rows, fields] = await pool.query("SELECT date_information, valeur, type,informations_capteurs.id_capteur, capteurs.nom as nom_capteurs,capteurs.id_piece, piece.nom as nom_piece, superficie, type_capteurs.nom as type_capteurs, unité FROM informations_capteurs INNER JOIN capteurs ON informations_capteurs.id_capteur = capteurs.id INNER JOIN piece ON piece.id = capteurs.id_piece INNER JOIN type_capteurs ON capteurs.type = type_capteurs.id WHERE capteurs.id= ?", [id])
             res.json({
                 data: rows
         })
@@ -90,7 +93,7 @@ const postsController = {
     create: async (req, res) => {
         try{
             const { utilisateur, mdp, nom, prenom, mail } = req.body
-            // const sql = "insert into maison (nom, superficie) values (?,?)";
+            // const sql = "insert into piece (nom, superficie) values (?,?)";
             const sql = "INSERT INTO users SET utilisateur = ?, mdp = ?, nom = ?, prenom = ?, mail = ?, date_creation = NOW()"
             const [rows, fields] = await pool.query(sql, [utilisateur, mdp, nom, prenom, mail])
             res.json({
@@ -106,10 +109,10 @@ const postsController = {
 
     create_capteurs: async (req, res) => {
         try{
-            const { id_capteurs, information } = req.body
-            // const sql = "insert into maison (nom, superficie) values (?,?)";
-            const sql = "INSERT INTO informations_capteurs SET id_capteur = ?, information = ?, date_information=NOW()"
-            const [rows, fields] = await pool.query(sql, [id_capteurs, information])
+            const { id_capteurs, valeur } = req.body
+            // const sql = "insert into piece (nom, superficie) values (?,?)";
+            const sql = "INSERT INTO informations_capteurs SET id_capteur = ?, valeur = ?, date_information=NOW()"
+            const [rows, fields] = await pool.query(sql, [id_capteurs, valeur])
             res.json({
                 data: rows
             }) 
