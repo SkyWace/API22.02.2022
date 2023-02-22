5de27a7fe6f5b8983181342c142fb6170f9a7611
SkyWace <simon.delcloque@hotmail.fr>
2023-02-22 12:11:42 +0100

    .

--- database/index.js
+++ database/index.js
@@ -10,7 +10,7 @@ const mysql = require('mysql2')
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'Projetcube2',
     database: 'cube2',
 });
 
