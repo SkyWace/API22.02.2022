50de1290b475f394a9a73ba20e55b813adf10238
SkyWace <simon.delcloque@hotmail.fr>
2023-02-23 10:22:18 +0100

    Test

--- controller/posts.controller.js
+++ controller/posts.controller.js
@@ -49,8 +49,9 @@ const postsController = {
         try {
           const { id_piece } = req.params;
       
-          const query = "SELECT ic.date_information, GROUP_CONCAT(IF(c.type = 1, ic.valeur, NULL) ORDER BY c.id SEPARATOR ', ') AS informations_type_temperature, GROUP_CONCAT(IF(c.type = 2, ic.valeur, NULL) ORDER BY c.id SEPARATOR ', ') AS informations_type_humidite FROM informations_capteurs ic JOIN capteurs c ON ic.id_capteur = c.id WHERE c.id_piece = ? GROUP BY ic.date_information DESC LIMIT 1";
-      
+          const query = "SELECT ic.date_information, GROUP_CONCAT(IF(c.type = 1, ic.valeur, NULL) ORDER BY c.id_capteur SEPARATOR ', ') AS informations_type_temperature, GROUP_CONCAT(IF(c.type = 2, ic.valeur, NULL) ORDER BY c.id_capteur SEPARATOR ', ') AS informations_type_humidite FROM informations_capteurs ic JOIN capteurs c ON ic.id_capteur = c.id WHERE c.id_piece = ? AND c.type = 1 AND ic.date_information IN (SELECT MAX(date_information)FROM informations_capteursWHERE id_capteur = c.id)GROUP BY ic.date_information DESC LIMIT 1";
+   
+
           const params = [id_piece];
       
           const [rows, fields] = await pool.query(query, params);
