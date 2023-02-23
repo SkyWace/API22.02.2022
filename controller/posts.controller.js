1d2b87280126941f1d3027be2708dd39bdcc29eb
SkyWace <simon.delcloque@hotmail.fr>
2023-02-23 10:42:14 +0100

    Test

--- controller/posts.controller.js
+++ controller/posts.controller.js
@@ -49,7 +49,7 @@ const postsController = {
         try {
           const { id_piece } = req.params;
       
-          const query = "SELECT ic.date_information, GROUP_CONCAT(IF(c.type = 1, ic.valeur, NULL) ORDER BY c.id SEPARATOR ', ') AS informations_type_temperature, GROUP_CONCAT(IF(c.type = 2, ic.valeur, NULL) ORDER BY c.id SEPARATOR ', ') AS informations_type_humidite FROM informations_capteurs ic JOIN capteurs c ON ic.id_capteur = c.id WHERE c.id_piece = ? GROUP BY ic.date_information DESC LIMIT 1";
+          const query = "SELECT ic.date_information, GROUP_CONCAT(IF(c.type = 1, ic.valeur, NULL) ORDER BY c.id SEPARATOR ', ') AS informations_type_temperature, GROUP_CONCAT(IF(c.type = 2, ic.valeur, NULL) ORDER BY c.id SEPARATOR ', ') AS informations_type_humidite FROM informations_capteurs ic JOIN capteurs c ON ic.id_capteur = c.id WHERE c.id_piece = ? GROUP BY ic.date_information DESC";
           
           const params = [id_piece];
       
@@ -76,7 +76,8 @@ const postsController = {
             status: "error",
           });
         }
-      },      
+      },
+      
     
 
       get_information: async (req, res) => {
