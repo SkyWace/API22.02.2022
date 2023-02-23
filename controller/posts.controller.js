6d23722b7545945bd5037e38be2ac4d689ba2d5b
SkyWace <simon.delcloque@hotmail.fr>
2023-02-23 09:54:33 +0100

    Update Get_information

--- controller/posts.controller.js
+++ controller/posts.controller.js
@@ -49,15 +49,14 @@ const postsController = {
         try {
           const { id_piece } = req.params;
       
-          const [tempRows] = await pool.query(
-            "SELECT ic.date_information, GROUP_CONCAT(ic1.valeur ORDER BY ic1.id_capteur SEPARATOR ', ') AS informations_type_temperature FROM informations_capteurs ic LEFT JOIN capteurs c1 ON ic.id_capteur = c1.id AND c1.type = 1 LEFT JOIN informations_capteurs ic1 ON ic1.id_capteur = c1.id AND ic.date_information = ic1.date_information WHERE c1.id_piece = ? GROUP BY ic.date_information DESC LIMIT 1",
-            [id_piece]
-          );
-      
-          const [humRows] = await pool.query(
-            "SELECT ic.date_information, GROUP_CONCAT(ic2.valeur ORDER BY ic2.id_capteur SEPARATOR ', ') AS informations_type_humidité FROM informations_capteurs ic LEFT JOIN capteurs c2 ON ic.id_capteur = c2.id AND c2.type = 2 LEFT JOIN informations_capteurs ic2 ON ic2.id_capteur = c2.id AND ic.date_information = ic2.date_information WHERE c2.id_piece = ? GROUP BY ic.date_information DESC LIMIT 1",
-            [id_piece]
-          );
+          const tempQuery = "SELECT ic.date_information, GROUP_CONCAT(ic1.valeur ORDER BY ic1.id_capteur SEPARATOR ', ') AS informations_type_temperature FROM informations_capteurs ic LEFT JOIN capteurs c1 ON ic.id_capteur = c1.id AND c1.type = 1 LEFT JOIN informations_capteurs ic1 ON ic1.id_capteur = c1.id AND ic.date_information = ic1.date_information WHERE c1.id_piece = ? GROUP BY ic.date_information DESC LIMIT 1";
+    const tempParams = [id_piece];
+
+    const humiditeQuery = "SELECT ic.date_information, GROUP_CONCAT(ic2.valeur ORDER BY ic2.id_capteur SEPARATOR ', ') AS informations_type_humidite FROM informations_capteurs ic LEFT JOIN capteurs c2 ON ic.id_capteur = c2.id AND c2.type = 2 LEFT JOIN informations_capteurs ic2 ON ic2.id_capteur = c2.id AND ic.date_information = ic2.date_information WHERE c2.id_piece = ? GROUP BY ic.date_information DESC LIMIT 1";
+    const humiditeParams = [id_piece];
+
+    const [tempRows, tempFields] = await pool.query(tempQuery, tempParams);
+    const [humiditeRows, humiditeFields] = await pool.query(humiditeQuery, humiditeParams);
       
           res.json({
             data: {
