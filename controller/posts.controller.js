4a020771b1c92b38d87580c3af4885ee2b52653c
SkyWace <simon.delcloque@hotmail.fr>
2023-02-23 10:35:39 +0100

    Test

--- controller/posts.controller.js
+++ controller/posts.controller.js
@@ -79,20 +79,38 @@ const postsController = {
       },      
     
 
-    get_informations: async (req, res) => {
-        try{
-            const{id_piece} = req.params
-            const [rows, fields] = await pool.query("SELECT ic.date_information, GROUP_CONCAT(ic1.valeur ORDER BY ic1.id_capteur SEPARATOR ', ') AS informations_type_humidité, GROUP_CONCAT(ic2.valeur ORDER BY ic2.id_capteur SEPARATOR ', ') AS informations_type_température FROM informations_capteurs ic LEFT JOIN capteurs c1 ON ic.id_capteur = c1.id AND c1.type = 1 LEFT JOIN informations_capteurs ic1 ON ic1.id_capteur = c1.id AND ic.date_information = ic1.date_information LEFT JOIN capteurs c2 ON ic.id_capteur = c2.id AND c2.type = 2 LEFT JOIN informations_capteurs ic2 ON ic2.id_capteur = c2.id AND ic.date_information = ic2.date_information WHERE c1.id_piece = ? GROUP BY ic.date_information", [id_piece])
-            res.json({
-                data: rows
-        })
+      get_informations: async (req, res) => {
+        try {
+          const { id_piece } = req.params;
+      
+          const query = "SELECT ic.date_information, GROUP_CONCAT(IF(c.type = 1, ic.valeur, NULL) ORDER BY c.id SEPARATOR ', ') AS informations_type_temperature, GROUP_CONCAT(IF(c.type = 2, ic.valeur, NULL) ORDER BY c.id SEPARATOR ', ') AS informations_type_humidite FROM informations_capteurs ic JOIN capteurs c ON ic.id_capteur = c.id WHERE c.id_piece = ? GROUP BY ic.date_information";
+          
+          const params = [id_piece];
+      
+          const [rows, fields] = await pool.query(query, params);
+      
+          let informations_type_temperature = null;
+      
+          if (rows[0].informations_type_temperature) {
+            informations_type_temperature = rows[0].informations_type_temperature;
+          } else {
+            const random_temperature = Math.floor(Math.random() * (25 - 15 + 1) + 15);
+            informations_type_temperature = `${random_temperature}`;
+          }
+      
+          res.json({
+            data: {
+              informations_type_temperature,
+              informations_type_humidite: rows[0].informations_type_humidite || null,
+            },
+          });
         } catch (error) {
-            console.log(error)
-            res.json({
-                status: "error"
-        })
+          console.log(error);
+          res.json({
+            status: "error",
+          });
         }
-    },
+      },      
 
     getById: async (req, res) => {
         try{
